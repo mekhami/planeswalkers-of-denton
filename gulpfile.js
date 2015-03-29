8aebe58dd88cc12db265267041293d0967fdf57e
Lawrence <lawrence.vanderpool@gmail.com>
2015-03-29 15:42:30 -0400

    restructured again for simplicity

--- gulpfile.js
+++ gulpfile.js
@@ -12,38 +12,44 @@ var gulp = require('gulp'),
     livereload  = require('gulp-livereload'),
     del  = require('del');
 
+var css_source_files = "planeswalkers/static_source/css/**/*.css"
+var sass_source_files = "planeswalkers/static_source/sass/**/*.scss"
+var js_source_files = "planeswalkers/static_source/js/**/*.js"
+var image_source_files = "planeswalkers/static_source/images/**/*"
+var static_dest = 'planeswalkers/static/'
+
 gulp.task('styles', function() {
-    return gulp.src('<<css source files>>')
-        .pipe(sass({'<<sass source files>>'))
+    return gulp.src(css_source_files)
+        .pipe(sass(sass_source_files))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('<<django static dir>>'))
+        .pipe(gulp.dest(static_dest))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest('<<django static dir>>'))
+        .pipe(gulp.dest(static_dest))
         .pipe(notify({ message: 'Styles task complete.' }));
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('<<js source files>>')
+    return gulp.src(js_source_files)
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('django static js dir'))
+        .pipe(gulp.dest(static_dest))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('<<django static js dir>>'))
+        .pipe(gulp.dest(static_dest))
         .pipe(notify({ message: "Scripts task complete.' }));
 });
 
 gulp.task('images', function() {
-    return gulp.src('<<image source files>>')
+    return gulp.src(image_source_files)
         .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('<<django images dir>>'))
+        .pipe(gulp.dest(static_dest))
         .pipe(notify({ message: "Images task complete." }));
 });
 
 gulp.task('clean', function(cb) {
-    del(['<<django static css dir>>', '<<django static js dir>>', '<<django static images dir>>'], cb)
+    del([static_dest], cb)
 });
 
 gulp.task('default', ['clean'], function() {
@@ -51,7 +57,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('<<sass source files>>', ['styles']);
-    gulp.watch('<<js source files>>', ['scripts']);
-    gulp.watch('<<image source files>>', ['images']);
+    gulp.watch(sass_source_files, ['styles']);
+    gulp.watch(js_source_files, ['scripts']);
+    gulp.watch(image_source_files, ['images']);
 });
